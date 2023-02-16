b9562f63422bed39701009b1359886f259d31af5
Bujdos Dániel <sonydanca@gmail.com>
2023-02-16 12:06:57 +0100

    navbae

---
+++ piac.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    h1{
+        text-align: center;
+        font-size: 10cm;
+
+    }
+  </style>
+<body>
+<div class="controler">
+    <h1>Piac in php</h1>
+  </div>  
+</body>
+</html>
