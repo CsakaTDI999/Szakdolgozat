ea1cda1abf112a164a0396d59bd4e23b1854e22a
Bujdos Dániel <sonydanca@gmail.com>
2023-02-23 16:27:11 +0100

    naggyonkomoly index de meg bugos

--- index.php
+++ index.php
@@ -7,47 +7,72 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-  
+  <style>
+    .content{
+      position: absolute;
+      top: calc(50% - 120px);
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+      z-index: 1;
+    }
+
+    img {
+      width: 55%;
+    }
+
+    .content h1{
+      font-size: 90px;
+      color: white;
+      font-weight: 600;
+    }
+    
+    .indexvideo{
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      z-index: -1;
+    }
+
+    .login-btn {
+      margin-right: 10px;
+      margin-right: 10px;
+      border-color: red;
+    }
+
+    @media (max-aspect-ratio:16/9){
+      .indexvideo{
+        width: auto;
+        height: 100%;
+      }
+    }
+
+  </style>
 </head>
 <body>
+  <video autoplay loop muted plays-inline class="indexvideo">
+    <source src="indexvideo.mp4" type="video/mp4">
+  </video>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img src="logo.png" class="img-fluid"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <div class="content">
+    <img src="logo.png" alt="Your Image"><br><br>
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        Termék állapota
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item"> 
-              <a class="nav-link" aria-current="page" href="index.php">Rólunk</a>
-              </li>
-              <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Alkatrészek
-              </a>
-              <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="ujalkatreszek.php">Új</a></li>
-                  <li><a class="dropdown-item" href="piac.php">Használt</a></li>
-              </ul>
-              </li>
-          </ul>
-          <ul class="navbar-nav me-0 mb-2 mb-lg-0">
-              <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="regisztracio.php">Regisztráció</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="Belepes.php">Belépés</a>
-              </li>
-          </ul>
-      </div>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="ujalkatreszek.php">Új</a></li>
+        <li><a class="dropdown-item" href="piac.php">Használt</a></li>
+      </ul>
     </div>
-  </nav>
-    
-  <div class="container">
-    <h3>Rólunk:</h3>
-    <p>asdasd sokszöveg asd.</p>
   </div>
+
+  <div class="d-flex justify-content-end align-items-center p-3">
+    <a href="Belepes.php" class="btn btn-outline-light login-btn">Belépés</a>
+    <a href="regisztracio.php" class="btn btn-primary btn-danger">Regisztráció</a>
+
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
-  </html>
+</body>
+</html>
