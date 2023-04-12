11b51fe1446605605b04ab60159c0d8d21abb33d
Bujdos Dániel <sonydanca@gmail.com>
2023-04-12 11:09:19 +0200

    Update uj_torles.php

---
+++ uj_torles.php
@@ -0,0 +1,22 @@
+<?php
+require_once('kapcsolat.php');
+
+if (!isset($_SESSION['ID']) || !isset($_SESSION['admin']) || $_SESSION['admin'] != 1) {
+    header('Location: Belepes.php');
+    exit();
+}
+
+if (isset($_GET['table']) && isset($_GET['id'])) {
+    $selected_table = $_GET['table'];
+    $id = $_GET['id'];
+
+    $stmt = $conn->prepare("DELETE FROM $selected_table WHERE id = ?");
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+
+   
+} else {
+    header("Location: ujalkatreszek_szerkesztese.php");
+    exit();
+}
+?>
